6aa8ca327aa694a1d8b9ef879777c06836867f40
Yury Pavlov <ympavlov@gmail.com>
2022-08-12 20:18:08 +0300

    fix duplicate synchronized

--- src/main/java/com/github/ympavlov/study/jmm/darkmagic/prosuming/SynchronizedClean.java
+++ src/main/java/com/github/ympavlov/study/jmm/darkmagic/prosuming/SynchronizedClean.java
@@ -5,20 +5,16 @@ public class SynchronizedClean implements Holder
 	private State value;
 
 	@Override
-	public void set(State v)
+	public synchronized void set(State v)
 	{
-		synchronized (this) {
-			value = v;
-		}
+	    value = v;
 	}
 
 	@Override
 	public synchronized State get()
 	{
 		State v;
-		synchronized (this) {
-			v = value;
-		}
+		v = value;
 		return v;
 	}
 }
